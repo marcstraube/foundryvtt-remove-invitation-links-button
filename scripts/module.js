819b5ec869ec9edb035099989397df11b928a24a
Marc Straube <email@marcstraube.de>
2024-07-07 14:15:47 +0200

    Remove deprecated libChangelogs code.

--- scripts/module.js
+++ scripts/module.js
@@ -1,15 +1,3 @@
-Hooks.once('libChangelogsReady', function () {
-    libChangelogs.register(
-        "remove-invitation-links-button",
-        "<ul>" +
-        "<li>Fix deprecated field warning.</li>" +
-        "<li>Fix issue that does not remove button Remove invitation links button via stylesheet instead of script to fix timing issue.</li>" +
-        "<li>Compatibility fix for TidyUI.</li>" +
-        "</ul>",
-        "minor"
-    );
-});
-
 Hooks.on("renderSidebarTab", function (application, html) {
     if (typeof game.modules.get("tidy-ui_game-settings") !== "undefined"
         && game.modules.get("tidy-ui_game-settings").active) {
