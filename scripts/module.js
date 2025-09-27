2c511efdca8bfd33803847c84c5cdf5d5c1419cb
Marc Straube <email@marcstraube.de>
2025-09-27 20:42:14 +0200

    Update for FoundryVTT v13.

--- scripts/module.js
+++
@@ -1,6 +0,0 @@
-Hooks.on("renderSidebarTab", function (application, html) {
-    if (typeof game.modules.get("tidy-ui_game-settings") !== "undefined"
-        && game.modules.get("tidy-ui_game-settings").active) {
-        $(html).find("div#settings-access button[data-action='logout']").addClass("tidyui-fix");
-    }
-});
